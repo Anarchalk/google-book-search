7c7847a5b7994cd69e386aab8b221894add43fa5
Anar Chalk <annachak9@gmail.com>
2020-03-13 22:30:54 -0400

    finalized one page code

--- src/App.js
+++ src/App.js
@@ -1,66 +1,78 @@
-import React, { Component } from 'react';
-import Header from './component/Header';
+import React, { Component } from "react";
+import Header from "./component/Header";
 //import PrintType from './component/PrintType';
-import './App.css';
+import "./App.css";
 
 //https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=yourAPIKey
 
 class App extends Component {
-
   state = {
-    books:[],
-    search: '',
+    books: [],
+    search: "",
     //default value
-    printtype: 'All'
-  }
+    printType: "All",
+    filter: "No Filter"
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     // const baseUrl= 'https://www.googleapis.com/books/v1/volumes?q='
     // const query = 'subject+Henry'
     // const key = '&key=AIzaSyALZcoWIcBmUxohF8HfVzYf-JFBV4JCbC4'
     // const url = baseUrl + query + key
-
     // fetch(url)
     // .then(res => res.json())
     // .then(data=> {
-    //   console.log(data) 
+    //   console.log(data)
     //   this.setState({
     //     books:data.items
     //   })
     // })
   }
 
-  //handle print type filter search 
-  //all the values to be store 
+  //handle print type filter search
+  //all the values to be stored
 
-  handlePrintType = (e) => {
+  handleSelectChange = (e) => {
     e.preventDefault();
-    this.setState()
-  }
+    this.setState({
+      printType: e.target.value
+    });
+  };
 
-  handleSearch = (e) => {
+  handleBookType = (e) => {
+    e.preventDefault();
+    this.setState ({
+      filter: e.target.value
+    });
+  };
+
+  handleInputChange = (e) => {
     console.log(e.target.value);
     this.setState({
       search: e.target.value
-    })
-  }
+    });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const baseUrl= 'https://www.googleapis.com/books/v1/volumes?q='
-    const query = `subject+${this.state.search}`
-    const key = '&key=AIzaSyALZcoWIcBmUxohF8HfVzYf-JFBV4JCbC4'
-    const url = baseUrl + query + key
+    const baseUrl = "https://www.googleapis.com/books/v1/volumes?";
+    const searchParam = `q=subject+${this.state.search}`;
+    const keyParam = "key=AIzaSyALZcoWIcBmUxohF8HfVzYf-JFBV4JCbC4";
+    const typeParam = `printType=${this.state.printType}`;
+    const bookType = `filter=${this.state.filter}`
+    const url = baseUrl + searchParam + "&" + keyParam + "&" + typeParam + "&" + bookType;
+
+    console.log(url);
 
     fetch(url)
-    .then(res => res.json())
-    .then(data=> {
-      console.log(data) 
-      this.setState({
-        books:data.items
-      })
-    })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          books: data.items
+        });
+      });
+  };
 
   render() {
     return (
@@ -69,10 +81,11 @@ class App extends Component {
 
         <section className="search-bar">
           <form onSubmit={this.handleSubmit}>
+            Search:
             <input
               type="text"
               value={this.state.search}
-              onChange={this.handleSearch}
+              onChange={this.handleInputChange}
             />
             <input type="submit" value="Search" />
           </form>
@@ -80,25 +93,39 @@ class App extends Component {
         <section className="print-type-bar">
           <form onSubmit={this.handleSubmit}>
             <label>
-              Print Type: 
-              <select value={this.state.value} onChange={this.handleChange}>
+              Print Type:
+              <select onChange={this.handleSelectChange}>
                 <option value="All">All</option>
-                <option value="Books">Books</option>
+                <option value="books">books</option>
               </select>
             </label>
           </form>
         </section>
-        <section className="filter-bar"></section>
+        <section className="book-type-bar">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Book Type:
+              <select onChange={this.handleBookType}>
+                <option values="no-filter">No Filter</option>
+                <option values="ebooks">ebooks</option>
+              </select>
+            </label>
+          </form>
+        </section>
+        <section className="search-bar"></section>
         <main>
           <ul>
             {this.state.books.length &&
               this.state.books.map((book) =>
                 book.volumeInfo.description ? (
                   <li key={book.id}>
+                    <img src={book.volumeInfo.imageLinks.thumbnail}></img>
                     <h1>{book.volumeInfo.title}</h1>
+                    <p>Author: {book.volumeInfo.authors}</p>
+                    <p>Price: {book.saleInfo.listPrice.amount}</p>
                     <p>{book.volumeInfo.description}</p>
                   </li>
-                ) : null
+                ): null
               )}
           </ul>
         </main>
